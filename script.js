793db0e70c1e8168e2133e544eb02506e5411554
diaa <touncara.fatoumata@gmail.com>
2025-05-04 20:30:00 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+const prev_btn = document.querySelector(".control-previous");
+const next_btn = document.querySelector(".control-next");
+
+// Initialisation : affiche la première image lorsque n est égal à 0
+let n = 0; // quand c'est à 0, ça affiche la première image
+/**
+ * Change le slide des images en fonction de l'index spécifié.
+ * @param {number} index - L'index de l'image à afficher.
+ */
+function changeSlide() {
+  for (let i = 0; i < imgs.length; i++) {
+    // Cache toutes les images
+    imgs[i].style.display = "none";
+  }
+  // Affiche l'image à l'index spécifié
+  imgs[n].style.display = "block";
+}
+changeSlide();
+
+prev_btn.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+  changeSlide();
+});
+
+next_btn.addEventListener("click", (e) => {
+  if (n < imgs.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlide();
+});
